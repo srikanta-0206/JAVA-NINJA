b32dc8bbb9726e2985052fb91cabfa40fba00525
srikanta-0206 <srikantasahu280@gmail.com>
2025-09-24 21:26:36 +0530

    Patterns

---
+++ src/com/pattern/practice/Pattern10.java
@@ -0,0 +1,34 @@
+package com.pattern.practice;
+//butterfly
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        for (int i=n-1;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int k=1;k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
