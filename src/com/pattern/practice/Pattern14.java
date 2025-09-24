3ff816cda302db80e709b588ce42410e20877174
srikanta-0206 <srikantasahu280@gmail.com>
2025-09-24 21:27:05 +0530

    Patterns

---
+++ src/com/pattern/practice/Pattern14.java
@@ -0,0 +1,25 @@
+package com.pattern.practice;
+//diamond
+public class Pattern14 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
