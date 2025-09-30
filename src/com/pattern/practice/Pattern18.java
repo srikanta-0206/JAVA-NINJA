e7768cb0fb1b0e895307de8e4d737942badade16
Srikanta Sahu <sriikantasahu280@gmail.com>
2025-09-30 12:00:58 +0530

    chnge

--- src/com/pattern/practice/Pattern18.java
+++ src/com/pattern/practice/Pattern18.java
@@ -3,16 +3,7 @@ package com.pattern.practice;
 public class Pattern18 {
     public static void main(String[] args) {
         int n = 5;
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=n-i;j++){
-                System.out.print(" ");
-                for (int k=1;k<=i;k++){
-
-                    System.out.print(j);
-                }
-            }
-
-            System.out.println();
+        for (int i = 1; i <= n; i++) {
 
         }
 
