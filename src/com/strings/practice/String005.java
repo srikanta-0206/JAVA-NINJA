4ea2d4c03a2f01bd1ed5a57ef8308d44b7883394
Srikanta Sahu <sriikantasahu280@gmail.com>
2025-09-30 14:55:10 +0530

    Strings questions

---
+++ src/com/strings/practice/String005.java
@@ -0,0 +1,13 @@
+package com.strings.practice;
+
+//5) WAJP to Sum Digits in an Alphanumeric String
+//Logic: Traverse string, if char is digit → add to sum.
+//        Example:
+//Input: "JSPIDER@123"
+//Output: 6
+public class String005 {
+
+    public static void main(String[] args) {
+
+    }
+}
