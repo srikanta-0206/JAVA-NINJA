08071057ac372f978bc93f36e1c0ffda2d02a347
Srikanta Sahu <srikantasahu280@gmail.com>
2025-10-01 13:41:14 +0530

    Strings questions

---
+++ src/com/strings/practice/String007.java
@@ -0,0 +1,24 @@
+package com.strings.practice;
+//7) WAJP to Convert Uppercase to Lowercase
+//Logic: Use toLowerCase() function.
+//Example:
+//Input: "HELLO"
+//Output: "hello"
+
+//        ---
+public class String007 {
+    public static void main(String[] args) {
+       String sstr = "hello";
+       String s = "";
+       for (int i=0;i<=sstr.length()-1;i++){
+          char a = sstr.charAt(i);
+          if(a>='a'&& a<='z'){
+              a=(char)(a-32);
+          }
+          s+=a;
+       }
+        System.out.println(s);
+    }
+}
+
+
