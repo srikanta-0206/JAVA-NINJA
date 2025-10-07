b6503f85d81040be59026b798dbe0c6a08acb1d0
Srikanta Sahu <srikantasahu280@gmail.com>
2025-10-07 19:56:17 +0530

    Arrays

---
+++ src/com/ARRAYS/ARRAY.java
@@ -0,0 +1,30 @@
+package com.ARRAYS;
+
+import java.util.Scanner;
+
+public class ARRAY {
+public static void main(String[] args) {
+//	int marks[] = new int[70];
+//	int numbers[] = {1,2,3,4,};
+//	int sasa[] = {4,5,6};
+//	String fruits[] = {"appel","mango"};
+//	System.out.println(fruits);
+	
+	
+	int marks [] = new int [100];
+	Scanner sc = new Scanner(System.in);
+	marks[0] = sc.nextInt();
+	marks[1] = sc.nextInt();
+	marks[2] = sc.nextInt();
+	
+	System.out.println(marks[0] );
+	System.out.println(marks[1]);
+	System.out.println(marks[2]);
+	
+	marks[2] = marks[2]+30;
+	System.out.println(marks[2]);
+	
+
+}
+
+}
