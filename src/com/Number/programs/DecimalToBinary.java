82ce16b02a11fd2436d8ce8e69d95bd6c087cd5f
Srikanta Sahu <srikantasahu280@gmail.com>
2025-10-07 19:44:30 +0530

    DEC

---
+++ src/com/Number/programs/DecimalToBinary.java
@@ -0,0 +1,19 @@
+package com.Number.programs;
+
+public class DecimalToBinary {
+public static void DectoBin(int n){
+	int pow=0;
+	int bin=0;
+	 while(n>0){
+		 int rem = n%2;
+		 bin = bin+(rem * (int)Math.pow(10,pow));
+		 pow++;
+		 n=n/2;
+	 }
+	System.out.println(bin);
+}
+
+public static void main(String[] args) {
+DectoBin(12);
+}
+}
