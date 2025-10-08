d8af49e40eb8d8c78cef608274f06efefd99da68
Srikanta Sahu <srikantasahu280@gmail.com>
2025-10-08 11:32:47 +0530

    SEARCHING ALOG

---
+++ src/com/Searching/BinarySearch.java
@@ -0,0 +1,29 @@
+package com.Searching;
+
+public class BinarySearch {
+
+
+public static int Binary(int arr[],int key){
+	int start  = 0;
+	int end = arr.length-1;
+	while(start<=end){
+		int mid = (start + end)/2;
+		 if(arr[mid] == key){
+			 return mid;
+		 }
+		 if (arr[mid]<key){
+			 start = mid+1;
+		 }
+		 else {
+			 end = mid-1;
+		 }
+	}
+return -1;
+
+}
+public static void main(String[] args) {
+	int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+	int ans = Binary(arr,15);
+	System.out.println(ans);
+}
+}
